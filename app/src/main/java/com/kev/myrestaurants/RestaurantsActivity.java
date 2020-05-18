ffaba1702c446321907abb2191f1d34aab928b82
kev87ian <kelvinian87@gmail.com>
2020-05-18 23:35:28 +0300

    user input

---
+++ app/src/main/java/com/kev/myrestaurants/RestaurantsActivity.java
@@ -0,0 +1,43 @@
+//package com.kev.myrestaurants;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.widget.TextView;
+//
+//public class RestaurantsActivity extends AppCompatActivity {
+//    private TextView mLocationTextView;
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_restaurants);
+//        Intent intent = getIntent();
+//        String location = intent.getStringExtra("location");
+//        mLocationTextView.setText("Here are all the restaurants near: " + location );
+//    }
+//}
+
+package com.kev.myrestaurants;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RestaurantsActivity extends AppCompatActivity {
+    private TextView mLocationTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurants);
+        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+        mLocationTextView.setText("Here are all the restaurants near: " + location);
+    }
+}
